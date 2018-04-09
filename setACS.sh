0763bbf183a592459b45112028a3138b732b1db3
CH <ginsongsong@honghutech.com>
2018-04-09 14:35:55 +0800

    Create setACS.sh
    Verify the Broadcom 9797 and 8764 on this script

---
+++ setACS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for y in ` lspci -vvv| grep "Root Port" | grep PCI |cut -c1-7 `;
+do
+        cpu="0x0" #------- => cpu=0x0 , +++++-- => cpu=0x1f , +-+++-- => cpu=0x1d
+        echo "CPU setACS : $cpu"
+        setpci -s $y  116.w=$cpu
+        lspci -s $y
+        lspci -vvv -s $y | grep ACSCtl  #輸出目前PLX更改的結果
+done
+for y in ` lspci   | grep PLX |grep bridge|cut -c1-7 `;
+do
+                plx="0x0"  #------- => cpu=0x0 , +++++++ => cpu=0x7f , +-+++-- => cpu=0x1d
+                echo "PLX setACS : $plx"
+                setpci -s $y f2a.w=$plx
+                lspci -s $y
+                lspci -vvv -s $y | grep ACSCtl  #輸出目前PLX更改的結果
+
+done
